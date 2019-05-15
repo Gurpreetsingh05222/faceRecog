a323e774b6ba0e33cde99792ed362e9985253c41
unknown <gurpreetsingh05222@gmail.com>
2019-05-15 09:00:11 +0530

    added faceRecognition component and functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import React, {Component} from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: '856ef67b1d504e0e835b45d26a585669'
+});
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
+
   render(){
     return (
       <div className="App">
         <Navigation />
         <Logo />
-         {/*<ImageLinkForm />
-         <FaceRecognition /> */}
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
+    return(
+        <div>
+            <p className="f3">
+                {'This App will detect faces in pictures you upload.'}
+            </p>
+            <div className='center w-60'>
+                <div className='pa4 br3 shadow-5'>
+                    <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
